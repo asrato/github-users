3b87238168eed18791a1ee1cdd643a1e0963b00c
André Rato <9asrato@gmail.com>
2022-06-21 01:32:17 +0100

    dummy

---
+++ src/components/extras/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="footer-text">Made with React.js</p>
+            <img src='./assets/react.svg' className='footer-icon' />
+            <a href="https://github.com/asrato" target={'_blank'} className='footer-link'>
+                <img src='./assets/github.svg' className='footer-icon' />
+            </a>
+            <p className="footer-text">by André da Silva Rato</p>
+        </footer>
+    );
+};
+
+export default Footer;
